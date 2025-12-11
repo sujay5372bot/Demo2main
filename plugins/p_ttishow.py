bcd1e91e8c70173b1da6e300542b96a4769a6cb5
sujay5372bot <sujay53721@gmail.com>
2025-12-11 08:57:11 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -7,6 +7,7 @@ from Script import script
 from pyrogram import Client, filters, enums
 from pyrogram.errors import ChatAdminRequired, FloodWait
 from pyrogram.types import *
+from database.groupdb import *
 from database.ia_filterdb import Media, Media2, get_file_details, unpack_new_file_id, get_bad_files, db as clientDB, db2 as clientDB2
 from database.users_chats_db import db, delete_all_referal_users, get_referal_users_count, get_referal_all_users, referal_add_user
 from database.join_reqs import JoinReqs
@@ -14,6 +15,10 @@ from info import *
 from pyrogram.errors.exceptions.bad_request_400 import MessageTooLong, PeerIdInvalid
 from utils import get_settings, pub_is_subscribed, get_size, is_subscribed, save_group_settings, temp, verify_user, check_token, check_verification, get_token, get_shortlink, get_tutorial, get_seconds
 from database.connections_mdb import active_connection
+
+approved = await groupdb.is_approved(message.chat.id)
+if not approved:
+    return   # ❌ Do not send welcome message
 
 @Client.on_message(filters.new_chat_members & filters.group)
 async def save_group(bot, message):
