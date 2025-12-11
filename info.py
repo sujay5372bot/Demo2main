48f33fd4d6224fd03c66a8b1436569b0eac0cb17
sujay5372bot <sujay53721@gmail.com>
2025-12-11 16:55:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -33,7 +33,7 @@ SPELL_IMG = environ.get("SPELL_IMG", "https://te.legra.ph/file/15c1ad448dfe472a5
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002157033795'))
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6942557751 7336971189').split()]
 OWNER_ID = [int(owner) if id_pattern.search(owner) else owner for owner in environ.get('OWNER_ID', '6942557751 7336971189').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002148375787 -1002133505201').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002148375787 -1002133505201 -1002445219066').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '1798604396').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 
