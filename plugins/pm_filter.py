ca8f6783e9dcd64c9dd6c574af58a66dcaf50c31
sujay5372bot <sujay53721@gmail.com>
2026-01-08 20:22:47 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -378,6 +378,7 @@ async def advantage_spoll_choker(bot, query):
                     await bot.send_message(chat_id=LOG_CHANNEL, text=(script.NORSLTS.format(reqstr.id, reqstr.mention, movie)))
                 k = await query.message.edit(script.MVE_NT_FND, reply_markup=InlineKeyboardMarkup(button))
                 await asyncio.sleep(30)
+                await reply_msg.delete()
                 await k.delete()
 
 # Year 
