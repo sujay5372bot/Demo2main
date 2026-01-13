79027b77c1076e22c2299453ea106200e045711d
sujay5372bot <sujay53721@gmail.com>
2026-01-14 00:03:34 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2781,7 +2781,7 @@ async def auto_filter(client, name, msg, reply_msg, ai_search, spoll=False):
             search = re.sub(r"\b(pl(i|e)*?(s|z+|ease|se|ese|(e+)s(e)?)|((send|snd|giv(e)?|gib)(\sme)?)|movie(s)?|new|latest|bro|bruh|broh|helo|that|find|dubbed|link|venum|iruka|pannunga|pannungga|anuppunga|anupunga|anuppungga|anupungga|film|undo|kitti|kitty|tharu|kittumo|kittum|movie|any(one)|with\ssubtitle(s)?)", "", search, flags=re.IGNORECASE)
             search = re.sub(r"\s+", " ", search).strip()
             search = search.replace("-", " ")
-            search = search.replace(":", " ")
+            search = search.replace(":", "")
             search = search.replace(".", " ")
             search = search.replace("_", " ")
             files, offset, total_results = await get_search_results(message.chat.id ,search, offset=0, filter=True)
